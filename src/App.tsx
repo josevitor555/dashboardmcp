4bbbee7544d63659ffbee8d6168ff4b2967d4299
josevitor555 <koservitor123@gmail.com>
2025-07-20 21:58:08 -0300

    Dashboard example with Shadcn MCP

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+
+import './App.css';
+import './index.css';
+
+const App = () => {
+
+  return (
+    <>
+      <div className="app flex flex-col">
+        <h1 className='text-3xl font-bold underline text-white'> Hello :D </h1>
+        <button className='text-white mt-4'>
+          Click me!
+        </button>
+      </div>
+    </>
+  );
+}
+
+export default App;
